e51f0e680ca7332b7835cf5f5ffb08f737bbf46a
Nagesh Thakre <nageshrameshwar.thakre@concentrix.com>
2024-11-21 13:09:08 +0530

    stage modles

---
+++ models/src/stage/stg_customer.sql
@@ -0,0 +1,12 @@
+select distinct
+    customer_code,
+    customer_name,
+    address,
+    postal_code,
+    country,
+    {{ split_part('latlng', "','", 1) }} latitude,
+    {{ split_part('latlng', "','", 2) }} longitude,
+    contact_name,
+    phone,
+    email
+from {{ ref('stg_sales_data') }}
